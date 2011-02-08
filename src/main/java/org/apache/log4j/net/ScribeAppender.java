65efcd6a7a5f6cb58000773f057582f0414dc9f3
Josh Devins <info@joshdevins.net>
2011-02-08 17:09:10 +0100

    Package name change

--- src/main/java/org/apache/log4j/net/ScribeAppender.java
+++ src/main/java/org/apache/log4j/net/ScribeAppender.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.log4j.scribe;
+package org.apache.log4j.net;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.transport.TSocket;
