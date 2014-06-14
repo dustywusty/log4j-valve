76d933c81f0b851687d9cab8234ae023a0aee74c
clarkda <dusty@clarkda.com>
2014-06-13 22:08:17 -0500

    updating for new lifecycle api. these methods must set state for STARTING and STOPPING

--- src/main/java/org/apache/catalina/valves/BaseValve.java
+++ src/main/java/org/apache/catalina/valves/BaseValve.java
@@ -3,6 +3,7 @@ package org.apache.catalina.valves;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.LifecycleState;
 import org.apache.catalina.util.LifecycleSupport;
 
 public abstract class BaseValve extends ValveBase implements Lifecycle {
@@ -29,7 +30,7 @@ public abstract class BaseValve extends ValveBase implements Lifecycle {
 
     /**
      * Add a lifecycle event listener to this component.
-     * 
+     *
      * @param listener
      *        The listener to add
      */
@@ -47,7 +48,7 @@ public abstract class BaseValve extends ValveBase implements Lifecycle {
 
     /**
      * Remove a lifecycle event listener from this component.
-     * 
+     *
      * @param listener
      *        The listener to add
      */
@@ -59,19 +60,19 @@ public abstract class BaseValve extends ValveBase implements Lifecycle {
      * Prepare for the beginning of active use of the public methods of this
      * component. This method should be called after <code>configure()</code>,
      * and before any of the public methods of the component are utilized.
-     * 
+     *
      * @exception LifecycleException
      *            if this component detects a fatal error
      *            that prevents this component from being used
      */
-    public void start() throws LifecycleException {
+    public void startInternal() throws LifecycleException {
 
         // Validate and update our current component state
         if (started) {
             throw new LifecycleException("valve already started");
         }
 
-        lifecycle.fireLifecycleEvent(START_EVENT, null);
+        setState(LifecycleState.STARTING);
         started = true;
 
         afterStart();
@@ -81,19 +82,20 @@ public abstract class BaseValve extends ValveBase implements Lifecycle {
      * Gracefully terminate the active use of the public methods of this
      * component. This method should be the last one called on a given
      * instance of this component.
-     * 
+     *
      * @exception LifecycleException
      *            if this component detects a fatal error
      *            that needs to be reported
      */
-    public void stop() throws LifecycleException {
+    public void stopInternal() throws LifecycleException {
 
         // Validate and update our current component state
         if (!started) {
             throw new LifecycleException("valve not started");
         }
 
-        lifecycle.fireLifecycleEvent(STOP_EVENT, null);
+        setState(LifecycleState.STOPPING);
+
         started = false;
 
         afterStop();
